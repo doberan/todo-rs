d052fffc3912b5d8cf74a24cc3da5b9ea0d4f2af
doberan <doberankei@gmail.com>
2020-06-17 19:26:56 +0900

    add sample

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("hello");
+use std::io;
+use tui::Terminal;
+use tui::backend::CrosstermBackend;
+use tui::widgets::{Widget, Block, Borders};
+use tui::layout::{Layout, Rect, Constraint, Direction};
+
+fn main() -> Result<(), io::Error> {
+    let stdout = io::stdout();
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    terminal.clear()?;
+    let rect = terminal.size()?;
+    println!("{:?}", rect);
+    terminal.draw(|mut f| {
+        let chunks = Layout::default()
+            .direction(Direction::Horizontal)
+            .constraints([Constraint::Ratio(1, 10), Constraint::Ratio(10, 20)].as_ref())
+            .split(Rect {
+                x: 0,
+                y: 0,
+                width: 10,
+                height: 20,
+            });
+        let width = rect.width.to_string();
+        let block = Block::default()
+             .title(width.as_str())
+             .borders(Borders::ALL);
+        f.render_widget(block, chunks[0]);
+        let block = Block::default()
+             .title("Block 2")
+             .borders(Borders::ALL);
+        f.render_widget(block, chunks[1]);
+    })
 }
